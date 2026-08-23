f8d5a889636c269bdee6a6b360a0b6a9466fd34a
Devilitachi666 <devilumar35@gmail.com>
2026-08-23 14:35:02 +0530

    Create providers.js

---
+++ providers.js
@@ -0,0 +1,13 @@
+class ProviderService {
+    constructor(config = {}) {
+        this.config = config;
+    }
+
+    async getStreams(id, type, s = null, e = null) {
+        throw new Error(
+            'getStreams() must be implemented by the provider'
+        );
+    }
+}
+
+module.exports = ProviderService;
