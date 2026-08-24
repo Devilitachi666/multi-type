192df32a74643d8f0906cc23ccd54da2b17ad941
Devilitachi666 <devilumar35@gmail.com>
2026-08-24 14:34:28 +0530

    Fix URL assignment in getStreams method

--- provider-registry.js
+++ provider-registry.js
@@ -2,16 +2,12 @@ const ProviderService = require('./providers');
 
 class DemoProvider extends ProviderService {
   async getStreams(id, type, s = null, e = null) {
-    const url =
-      this.config.url ||
-      'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
-
     return [
       {
         stream: {
           id: `demo-${id}`,
           serverName: 'Demo HLS Server',
-          url,
+          url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
           type: 'hls',
           quality: '1080p',
           qualities: ['1080p'],
@@ -24,6 +20,7 @@ class DemoProvider extends ProviderService {
           ],
           subtitles: []
         },
+
         providerInfo: {
           id: 'demo',
           name: 'Demo Provider',
