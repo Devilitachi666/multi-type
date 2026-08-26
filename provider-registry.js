0b6a97386f5ae6c75b7c74d320348670d53691c9
Devilitachi666 <devilumar35@gmail.com>
2026-08-26 20:15:41 +0530

    Refactor provider registry to use null and empty arrays

--- provider-registry.js
+++ provider-registry.js
@@ -9,17 +9,11 @@ class DemoProvider {
           serverName: 'Demo HLS Server',
           url: 'https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8',
           type: 'hls',
-          quality: '1080p',
-          qualities: ['1080p'],
-          languages: ['English'],
-          audioTracks: [
-            {
-              language: 'English',
-              label: 'English'
-            }
-          ],
-          subtitles: []
-        },
+          quality: null,
+qualities: [],
+languages: [],
+audioTracks: [],
+subtitles: []
 
         providerInfo: {
           id: 'demo',
