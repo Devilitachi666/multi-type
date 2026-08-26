bb5741a543bcf92c305061ec195cb66cacf55a45
Devilitachi666 <devilumar35@gmail.com>
2026-08-26 20:24:59 +0530

    Update provider info and format in provider-registry

--- provider-registry.js
+++ provider-registry.js
@@ -10,20 +10,17 @@ class DemoProvider {
           url: 'https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8',
           type: 'hls',
           quality: null,
-qualities: [],
-languages: [],
-audioTracks: [],
-subtitles: []
+          qualities: [],
+          languages: [],
+          audioTracks: [],
+          subtitles: []
 
-        providerInfo: {
-          id: 'demo',
-          name: 'Demo Provider',
-          priority: 1
-        }
-      }
-    ];
-  }
-}
+                providerInfo: {
+                    id: 'demo',
+                    name: 'Apple HLS Demo Provider',
+                    priority: 1
+                }
+            }
 
 module.exports = {
   demo: DemoProvider
