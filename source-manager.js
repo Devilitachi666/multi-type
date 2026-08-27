aae04549d03eb1243dde7212b8fb81cec0525bda
Devilitachi666 <devilumar35@gmail.com>
2026-08-27 16:59:36 +0530

    Update source-manager.js

--- source-manager.js
+++ source-manager.js
@@ -2,12 +2,23 @@ const providerManager = require('./provider-manager');
 const streamManager = require('./stream-manager');
 
 class SourceManager {
-    async getSources(id, type, s, e) {
+    /**
+     * Resolves and processes iframe stream sources from active providers
+     * 
+     * @param {string} id - TMDB, IMDb, or MAL ID
+     * @param {string} type - Media type ('movie', 'tv', 'anime')
+     * @param {string|number|null} s - Season number
+     * @param {string|number|null} e - Episode number
+     * @param {string|null} provider - Target provider ID (optional)
+     * @returns {Promise<Array>} Normalized list of iframe stream objects
+     */
+    async getSources(id, type, s = null, e = null, provider = null) {
         const rawStreams = await providerManager.fetchAllStreams(
             id,
             type,
             s,
-            e
+            e,
+            provider
         );
 
         return streamManager.process(rawStreams);
